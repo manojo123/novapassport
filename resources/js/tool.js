dfe694efd9fba653f557b2b325284dbb8123baa2
Jorge Moura <jjorgeammoura@gmail.com>
2020-02-17 16:03:29 -0500

    Fixed Headers included jquery to work alone with the package

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,3 +1,12 @@
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+window.Vue = require('vue');
+
 Nova.booting((Vue, router, store) => {
     router.addRoutes([
         {
